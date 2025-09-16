4170cad11cf8668b718ab78be1d39377e5f3ca42
rohit3171999 <rohit3171999@gmail.com>
2025-09-16 16:33:08 +0530

    add markup in Testimonials.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HeroSection from "./components/HeroSection.jsx";
 import Footer from "./components/Footer.jsx";
 import AboutBook from "./components/AboutBook.jsx";
 import AuthorSection from "./components/AuthorSection.jsx";
+import Testimonials from "./components/Testimonials.jsx";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <HeroSection />
             <AboutBook/>
             <AuthorSection/>
+            <Testimonials/>
 
 
         </>
