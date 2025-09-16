40fffa51943ddff3f829130e0f6f16bfadcc4dbc
rohit3171999 <rohit3171999@gmail.com>
2025-09-16 16:28:28 +0530

    add markup in AuthorSection.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/HeroSection.jsx";
 import Footer from "./components/Footer.jsx";
 import AboutBook from "./components/AboutBook.jsx";
+import AuthorSection from "./components/AuthorSection.jsx";
 
 function App() {
     return (
         <>
             <HeroSection />
             <AboutBook/>
+            <AuthorSection/>
 
 
         </>
